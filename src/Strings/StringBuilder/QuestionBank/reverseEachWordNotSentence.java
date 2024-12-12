762dfa6d04e90cea507c0e6d37460de4b9df8381
itsme-swe <me.harshmehra07@gmail.com>
2024-12-12 06:03:23 +0530

    upodate

--- src/Strings/StringBuilder/QuestionBank/reverseEachWordNotSentence.java
+++ src/Strings/StringBuilder/QuestionBank/reverseEachWordNotSentence.java
@@ -1,12 +1,32 @@
 package Strings.StringBuilder.QuestionBank;
 
+import java.util.Scanner;
+
 public class reverseEachWordNotSentence {
 
     public static void main(String[] args) {
 
-        StringBuilder sb = new StringBuilder("I am Harsh Mehra");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter your string");
 
-        System.out.println(sb);
+            StringBuilder sb = new StringBuilder(sc.nextLine());
+
+            reverse(sb, 0, sb.length() - 1);
+
+            System.out.println(sb);
+        }
 
     }
+
+    public static void reverse(StringBuilder sb, int i, int j) {
+
+        while (i <= j) {
+            char temp = sb.charAt(i);
+            sb.setCharAt(i, sb.charAt(j));
+            sb.setCharAt(j, temp);
+            i++;
+            j--;
+        }
+    }
+
 }
