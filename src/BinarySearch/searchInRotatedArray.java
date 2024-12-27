1dea41a9581d9669d057e44a99bccc3391d5c81d
itsme-swe <me.harshmehra07@gmail.com>
2024-12-27 05:31:41 +0530

    update

--- src/BinarySearch/searchInRotatedArray.java
+++ src/BinarySearch/searchInRotatedArray.java
@@ -10,6 +10,8 @@
  * 
  * LeetCode: https://leetcode.com/problems/search-in-rotated-sorted-array/description/
  * 
+ * 🔸 This is not the optimized solution to solve this problem
+ * 
  */
 package BinarySearch;
 
@@ -21,6 +23,83 @@ public class searchInRotatedArray {
 
     int target = 0;
 
+    int pivot = search(num, target);
+
+    System.out.println(pivot);
+
+  }
+
+  public static void printArray(int[] arr) {
+
+    for (int ele : arr) {
+      System.out.print(ele + " ");
+    }
+    System.out.println();
+
+  }
+
+  public static int search(int[] nums, int target) {
+
+    int n = nums.length;
+
+    if (n <= 2) {
+      for (int i = 0; i < n; i++) {
+        if (nums[i] == target)
+          return i;
+      }
+      return -1;
+    }
+
+    int lo = 1;
+    int hi = n - 2;
+
+    int pivot = -1;
+
+    while (lo <= hi) {
+
+      int mid = lo + (hi - lo) / 2;
+
+      if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+        pivot = mid;
+        break;
+      } else if (nums[mid] < nums[mid - 1] && nums[mid] < nums[mid + 1]) {
+        pivot = mid - 1;
+        break;
+      } else if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1]) {
+        if (nums[mid] > nums[n - 1])
+          lo = mid + 1;
+        else
+          hi = mid - 1;
+      }
+    }
+
+    if (pivot == -1) {
+      return binarySearch(nums, 0, n - 1, target);
+    }
+
+    int left = binarySearch(nums, 0, pivot, target);
+    if (left != -1)
+      return left;
+
+    int right = binarySearch(nums, pivot + 1, n - 1, target);
+    return right;
+
+  }
+
+  public static int binarySearch(int[] arr, int lo, int hi, int target) {
+
+    while (lo <= hi) {
+
+      int mid = lo + (hi - lo) / 2;
+      if (arr[mid] == target)
+        return mid;
+      else if (arr[mid] > target)
+        hi = mid - 1;
+      else
+        lo = mid + 1;
+
+    }
+    return -1;
   }
 
 }
